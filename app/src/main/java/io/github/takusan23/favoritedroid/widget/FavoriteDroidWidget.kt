b1fdfea49388b95da9f12617a68311e1aa419edd
takusan23 <takumu1200@gmail.com>
2023-05-29 01:27:36 +0900

    更新ボタン

--- app/src/main/java/io/github/takusan23/favoritedroid/widget/FavoriteDroidWidget.kt
+++ app/src/main/java/io/github/takusan23/favoritedroid/widget/FavoriteDroidWidget.kt
@@ -13,6 +13,18 @@ import io.github.takusan23.favoritedroid.R
 /** バンディット問題を使ってアプリアイコンをウィジェットに表示する */
 class FavoriteDroidWidget : AppWidgetProvider() {
 
+    /** 更新ボタンの受け取り */
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+        context ?: return
+        when (intent?.getStringExtra(KEY_BROADCAST_TYPE)?.let { BroadcastType.find(it) }) {
+            BroadcastType.UPDATE -> updateAppWidget(context)
+            else -> {
+                // do nothing
+            }
+        }
+    }
+
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         updateAppWidget(context)
     }
@@ -25,18 +37,34 @@ class FavoriteDroidWidget : AppWidgetProvider() {
         // do nothing
     }
 
+    /** ブロードキャストの操作。更新操作しか無いけど */
+    private enum class BroadcastType(val action: String) {
+        UPDATE("io.github.takusan23.favoritedroid.widget.FavoriteDroidWidget.UPDATE");
+
+        companion object {
+            fun find(action: String) = values().firstOrNull { it.action == action }
+        }
+    }
+
     companion object {
+
+        private const val KEY_BROADCAST_TYPE = "type"
 
         /** ウィジェットを更新する */
         fun updateAppWidget(context: Context) {
             val views = RemoteViews(context.packageName, R.layout.favorite_droid_widget).apply {
                 val remoteViewsFactoryIntent = Intent(context, FavoriteDroidWidgetService::class.java)
                 setRemoteAdapter(R.id.widget_grid_view, remoteViewsFactoryIntent)
-                // アプリを押した時
-                val pendingIntent = FavoriteDroidAppStartActivity.createIntent(context).let {
+                // GridViewのセルを押した時
+                val templatePendingIntent = FavoriteDroidAppStartActivity.createIntent(context).let {
                     PendingIntent.getActivity(context, 10, it, PendingIntent.FLAG_MUTABLE) // MUTABLE じゃないと Adapter 側のが反映されない
                 }
-                setPendingIntentTemplate(R.id.widget_grid_view, pendingIntent)
+                setPendingIntentTemplate(R.id.widget_grid_view, templatePendingIntent)
+                // 更新ボタン。このクラスへブロードキャストを送る
+                val updateBroadcastPendingIntent = Intent(context, FavoriteDroidWidget::class.java)
+                    .apply { putExtra(KEY_BROADCAST_TYPE, BroadcastType.UPDATE.action) }
+                    .let { PendingIntent.getBroadcast(context, 11, it, PendingIntent.FLAG_IMMUTABLE) }
+                setOnClickPendingIntent(R.id.widget_update_button, updateBroadcastPendingIntent)
             }
 
             // 更新
